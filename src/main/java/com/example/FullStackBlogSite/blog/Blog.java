17233a9e5728e9176d0f3596dd6ce8dd5d89d58a
Kerem Büyükhamurkar <98040249+kerembuyukhamurkar@users.noreply.github.com>
2025-04-21 21:34:15 +0300

    Feat added id to getRequest

--- src/main/java/com/example/FullStackBlogSite/blog/Blog.java
+++ src/main/java/com/example/FullStackBlogSite/blog/Blog.java
@@ -2,7 +2,8 @@ package com.example.FullStackBlogSite.blog;
 
 import java.time.LocalDateTime;
 
-public record Blog(String title,
+public record Blog(Integer id,
+                   String title,
                    String content,
                    LocalDateTime createDate,
                    String creator
